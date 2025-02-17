41870ff6438dc0a58c9331713bf0067d9e091950
martin <zhouqiang_342601@sina.com>
2025-02-17 17:26:55 +0800

    auto construct blog

--- bin/auto-deploy.sh
+++ bin/auto-deploy.sh
@@ -1,8 +1,7 @@
 npm run docs:build
 cd docs/.vitepress/dist
 
-git init
 git add -A
 git commit -m "auto construct blog"
 
-git push -f https://github.com/jzplp/jzplp.github.io.git master:gh-pages
+git push -f origin
